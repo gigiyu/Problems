0366ffa2458e383071bc006ba23fcdc613f9031e
gigiyu <30333423+gigiyu@users.noreply.github.com>
2017-07-31 21:40:31 -0700

    Update 07_31_2017.cpp

--- 07_31_2017.cpp
+++ 07_31_2017.cpp
@@ -1,42 +1,26 @@
 /****************************************************************************
 34. Triangle  
 Follow up: 
-    - find all triplets can form a triangle. O(nlogn) ???
+    - find any one triplets can form a triangle. O(nlogn) 
     - find the number of triplets can form a triangle. O(n**2)
-    - whether allowed to use one number more than once. 
+    - find all triplets can form a triangle. O(n**3)
+    - whether allowed to use one number more than once. j=i or j=i+1
 */
 class Solution {
 public:
-    // 0. find any triplet compose one triangle
-    vector<int> getTriangle_0(vector<int>& nums) {
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i < nums.size(); i++) {
-            int j = i + 1, k = nums.size() - 1;
-            while(j < k) {
-                if (nums[i] + nums[j] > nums[k]) return {nums[i], nums[j], nums[k]};
-                j++;
+    // 1. O(nlogn) find any triplet compose one triangle
+    vector<int> getTriangle_1(vector<int>& nums) {
+        sort(nums.begin(), nums.end()); //O(nlogn)
+        for(int i = 0; i < nums.size() - 2; i++) {
+            int j = i + 1, k = i + 2; //O(n)
+            if (nums[i] + nums[j] > nums[k]) {
+                return {nums[i], nums[j], nums[k]};
             }
         }
         return {};
     }
-    // 1. O(nlogn) all triplets
-    vector<vector<int>> getTriangle_1(vector<int>& nums) {
-        vector<vector<int>> ret;
-        sort(nums.begin(), nums.end());
-        for(int i = 0; i < nums.size(); i++) {
-            int j = i + 1, k = nums.size() - 1;
-            while(j < k) {
-                if (nums[i] + nums[j] > nums[k]) {
-                    ret.push_back({nums[i], nums[j], nums[k]});
-                    return ret;
-                }
-                j++;
-            }
-        }
-        return ret;
-    }
-    // 2. one number one time.
-    //  the number of all triplets O(n**2) = O(n) * (O(n) + O(n)) = i * (j + k);
+    // 2. the number of all triplets O(n**2) = O(n) * (O(n) + O(n)) = i * (j + k);
+    //  one number one time.
     int getTriangle_2(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int ret = 0, n = nums.size();
@@ -45,13 +29,14 @@ public:
             for(int j = i + 1; j < n; j++) {
                 while(k < n && nums[i] + nums[j] > nums[k]) k++;
                 cout << i << " " << j << " " << k << endl;
-                for(int p = j + 1; p < k; p++) cout << "    " << nums[i] << " " << nums[j] << " " << nums[p] << endl;
+                //for(int p = j + 1; p < k; p++) cout << "    " << nums[i] << " " << nums[j] << " " << nums[p] << endl;
                 ret += k - j - 1;
             }
         }
         return ret;
     }
-    // 3. allowed to use one number more than once. the number of all triplets
+    // 3. the number of all triplets 
+    // allowed to use one number more than once. the number of all triplets
     int getTriangle_3(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int ret = 0, n = nums.size();
@@ -60,6 +45,7 @@ public:
             for(int j = i; j < n; j++) {
                 while(k < n && nums[i] + nums[j] > nums[k]) k++;
                 cout << i << " " << j << " " << k << endl;
+                // find all O(n**3)
                 for(int p = j; p < k; p++) cout << "    " <<  nums[i] << " " << nums[j] << " " << nums[p] << endl;
                 ret += k - j;
             }
